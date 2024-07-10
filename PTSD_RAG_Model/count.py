cbc8773bd4d180324618649b697aac018678f54c
Siddharth Shukla <139350635+SiddharthShukla48@users.noreply.github.com>
2024-07-10 19:41:46 +0530

    Add files via upload
    rag model for ptsd counselling

---
+++ PTSD_RAG_Model/count.py
@@ -0,0 +1,23 @@
+# importing required classes 
+from pypdf import PdfReader 
+
+# Open the PDF file
+file_path = '/Users/siddharthshukla/Library/CloudStorage/OneDrive-ManipalUniversityJaipur/Kaam Dhandha/ML/Projects/MediHacks/PTSD_RAG_Model /us_census/sodapdf-converted.pdf'
+pdf_file = open(file_path, 'rb')
+
+# Initialize PDF reader
+pdf_reader = PdfReader(pdf_file)
+
+# Extract text from each page
+text = ""
+for page_num in range(len(pdf_reader.pages)):
+    page = pdf_reader.pages[page_num]
+    text += page.extract_text()
+
+# Close the PDF file
+pdf_file.close()
+
+# Count the words
+word_count = len(text.split())
+
+print(f"The number of words in the PDF is: {word_count}")
